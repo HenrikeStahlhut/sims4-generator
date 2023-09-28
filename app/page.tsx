0d8af74d880dc5e4a9ed0006aca751f417309e0a
Henrike Stahlhut <henrike.st@icloud.com>
2023-09-28 12:13:09 +0200

    chore: finish setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import Header from "./components/Header/Header";
 
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <h1>Hello world</h1>
+      <Header />
+    </>
+  );
 }
