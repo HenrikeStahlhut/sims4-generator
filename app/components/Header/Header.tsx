0b2ea6624f0bbde173a32701acc77282a42546fd
Henrike Stahlhut <henrike.st@icloud.com>
2023-09-28 15:41:29 +0200

    chore(header): add logo, add minor styling

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,9 +1,39 @@
+"use client";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  background-color: lightblue;
+  height: 70px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const Nav = styled.ul`
+  width: 20em;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
 export default function Header() {
   return (
     <>
-      <div>
-        <h1 className="text-xl">Header</h1>
-      </div>
+      <HeaderContainer>
+        <h2>Logo</h2>
+        <Nav>
+          <li>
+            <a href="/">Randomizer</a>
+          </li>
+          <li>
+            <a href="/">Challenges</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+        </Nav>
+      </HeaderContainer>
     </>
   );
 }
