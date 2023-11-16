c5ae82c6c4ee1dd704709599064560e7b4eaac9f
Henrike Stahlhut <henrike.st@icloud.com>
2023-11-16 15:09:23 +0100

    chore(challenges): add challenge array

---
+++ lib/challenges.ts
@@ -0,0 +1,123 @@
+const challenges = [
+  {
+    id: 1,
+    title: "Legacy challenge",
+    type: "gameplay",
+    description: "This is the Sims 4 legacy challenge",
+  },
+  {
+    id: 2,
+    title: "Rags to Riches challenge",
+    type: "gameplay",
+    description: "This is the Sims 4 rags to riches challenge",
+  },
+  {
+    id: 3,
+    title: "100 Baby challenge",
+    type: "gameplay",
+    description: "This is the Sims 4 100 baby challenge",
+  },
+  {
+    id: 4,
+    title: "Not so berry challenge",
+    type: "gameplay",
+    description: "This is the Sims 4 not so berry challenge",
+  },
+  {
+    id: 4,
+    title: "Off the grid challenge",
+    type: "gameplay",
+    description: "This is the Sims 4 off the grid challenge",
+  },
+  {
+    id: 5,
+    title: "Every room is a different color",
+    type: "buildmode",
+    description: "This is the Sims 4 every room is a different color challenge",
+  },
+  {
+    id: 6,
+    title: "Black and white build challenge",
+    type: "buildmode",
+    description: "This is the Sims 4 black and white build challenge",
+  },
+  {
+    id: 6,
+    title: "Black and white CAS challenge",
+    type: "CAS",
+    description: "This is the Sims 4 black and white CAS challenge",
+  },
+  {
+    id: 5,
+    title: "Every room is a different budget",
+    type: "buildmode",
+    description:
+      "This is the Sims 4 every room is a different budget challenge",
+  },
+  {
+    id: 6,
+    title: "Inverted Colors CAS challenge",
+    type: "CAS",
+    description: "This is the Sims 4 Inverted Colors CAS challenge",
+  },
+  {
+    id: 6,
+    title: "Random genetics CAS challenge",
+    type: "CAS",
+    description: "This is the Sims 4 Random genetics CAS challenge",
+  },
+  {
+    id: 1,
+    title: "Nightmare Legacy challenge",
+    type: "gameplay",
+    description: "This is the Sims 4 Nightmare Legacy challenge",
+  },
+  {
+    id: 1,
+    title: "Black widow challenge",
+    type: "gameplay",
+    description: "This is the Sims 4 Black widow challenge",
+  },
+  {
+    id: 1,
+    title: "Vampire Legacy challenge",
+    type: "gameplay",
+    description: "This is the Sims 4 Vampire Legacy challenge",
+  },
+  {
+    id: 1,
+    title: "10 minute build challenge",
+    type: "buildmode",
+    description: "This is the Sims 4 10 minute build challenge",
+  },
+  {
+    id: 1,
+    title: "The Sims 4 Random Challenge by James Turner",
+    type: "buildmode",
+    description: "The Sims 4 Random Challenge by James Turner",
+  },
+  {
+    id: 1,
+    title: "Impossible micro home challenge",
+    type: "buildmode",
+    description: "The Sims 4 Impossible micro home challenge from lilsimsie",
+  },
+  {
+    id: 1,
+    title: "Cursed build challenge by Plumbella",
+    type: "buildmode",
+    description: "The Sims 4 Random Cursed build challenge by Plumbella",
+  },
+  {
+    id: 1,
+    title: "Super Sim challenge by James Turner",
+    type: "gameplay",
+    description: "The Sims 4 Super Sim challenge by James Turner",
+  },
+  {
+    id: 1,
+    title: "Get rich from ... challenge",
+    type: "gameplay",
+    description: "The Sims 4 Get rich from ... challenge",
+  },
+];
