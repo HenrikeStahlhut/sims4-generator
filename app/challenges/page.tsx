18c6ed41531f52880e6a35f25142425e46dddc0b
Henrike Stahlhut <henrike.st@icloud.com>
2023-11-16 13:18:24 +0100

    feat(challenges): add component

---
+++ app/challenges/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Header from "../components/Header/Header";
+
+// TODO: remove use client?
+
+export default function Challenges() {
+  return (
+    <>
+      <Header />
+      <p>This is the challenges page</p>
+    </>
+  );
+}
