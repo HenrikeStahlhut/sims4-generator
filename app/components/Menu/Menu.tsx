191f19c3d990b70431e3046ecc20cb1c655b3437
Henrike Stahlhut <henrike.st@icloud.com>
2023-11-16 12:32:37 +0100

    chore(header): finish nav/menu

--- app/components/Menu/Menu.tsx
+++ app/components/Menu/Menu.tsx
@@ -14,6 +14,10 @@ export const StyledNav = styled.div<{ open: boolean }>`
     text-decoration: none;
     padding: 0px 30px;
     font-size: 1.2em;
+
+    &:hover {
+      color: #7aa117;
+    }
   }
 
   @media (max-width: 600px) {
@@ -33,10 +37,6 @@ export const StyledNav = styled.div<{ open: boolean }>`
       padding: 30px 0px;
       font-size: 2em;
       font-weight: 500;
-
-      &:hover {
-        color: var(--dark-green);
-      }
     }
   }
 `;
@@ -63,86 +63,3 @@ export default function Menu() {
     </>
   );
 }
-
-// import { useState } from "react";
-// import MenuButton from "./MenuButton";
-// import styled from "styled-components";
-
-// const StyledNav = styled.div<{ open: boolean }>`
-//   overflow: hidden;
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: center;
-
-//   @media (max-width: 600px) {
-//     background-color: pink;
-//     display: ${({ open }) => (open ? "flex" : "none")};
-//     flex-direction: column;
-//     align-items: center;
-//     top: 0;
-//     right: 0;
-//     height: 100vh;
-//     width: 100vw;
-
-//     .li {
-//       color: black;
-//       text-decoration: none;
-//       padding: 30px 0px;
-//       font-size: 2em;
-
-//       &:hover {
-//         color: var(--dark-green);
-//       }
-//     }
-//   }
-// `;
-
-// const Nav = styled.ul`
-//   width: 20em;
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: space-between;
-
-//   li {
-//     font-weight: 700;
-//     :hover {
-//       color: #7aa117;
-//     }
-//   }
-
-//   @media (max-width: 600px) {
-//     width: 20em;
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: space-between;
-//   }
-// `;
-
-// export default function Menu() {
-//   const [menuOpen, setMenuOpen] = useState(false);
-
-//   return (
-//     <>
-//       <MenuButton open={menuOpen} setOpen={setMenuOpen} />
-//       <StyledNav open={menuOpen}>
-//         <Nav>
-//           <li>
-//             <a href="/" onClick={() => setMenuOpen(false)}>
-//               Randomizer
-//             </a>
-//           </li>
-//           <li>
-//             <a href="/" onClick={() => setMenuOpen(false)}>
-//               Challenges
-//             </a>
-//           </li>
-//           <li>
-//             <a href="/" onClick={() => setMenuOpen(false)}>
-//               About
-//             </a>
-//           </li>
-//         </Nav>
-//       </StyledNav>
-//     </>
-//   );
-// }
--- app/components/Menu/MenuButton.tsx
+++ app/components/Menu/MenuButton.tsx
@@ -7,19 +7,19 @@ type MenuButtonProps = {
 
 export const StyledOpenMenuButton = styled.button<{ open: boolean }>`
   @media (max-width: 600px) {
-    position: ${({ open }) => (open ? "absolute" : "relative")};
+    /* position: ${({ open }) => (open ? "absolute" : "relative")}; */
     padding: 0;
     z-index: 2;
     background: transparent;
     color: ${({ open }) => (open ? "white" : "black")};
 
     div {
-      position: relative;
       width: 2rem;
       height: 0.25rem;
       border-radius: 10px;
       margin-top: 0.4rem;
       background-color: ${({ open }) => (open ? "white" : "black")};
+
       transition: all 0.3s linear;
       transform-origin: 1px;
 
