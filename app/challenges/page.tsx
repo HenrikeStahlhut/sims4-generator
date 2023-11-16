c864085139aa92bdbf3438e38cac92c4dafd0095
Henrike Stahlhut <henrike.st@icloud.com>
2023-11-16 13:59:58 +0100

    chore(challenges): add introduction text and image

--- app/challenges/page.tsx
+++ app/challenges/page.tsx
@@ -1,13 +1,30 @@
 "use client";
 import Header from "../components/Header/Header";
+import styled from "styled-components";
+import Image from "next/image";
 
 // TODO: remove use client?
+
+const ChallengesIntroduction = styled.p`
+  color: red;
+`;
 
 export default function Challenges() {
   return (
     <>
       <Header />
-      <p>This is the challenges page</p>
+      <ChallengesIntroduction>
+        If you are tired of playing the same story with your Sims all the time,
+        you might want to try out some Sims 4 challenges to spice up your
+        gameplay. Here you will find a list of some of the most fun and maybe
+        difficult challenges!
+      </ChallengesIntroduction>
+      <Image
+        src="/sims4-challenges.png"
+        width={500}
+        height={500}
+        alt="An image of three sims"
+      ></Image>
     </>
   );
 }
