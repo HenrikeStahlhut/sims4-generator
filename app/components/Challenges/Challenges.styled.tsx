278273088a6ed47657923e91a250132a58372d79
Henrike Stahlhut <henrike.st@icloud.com>
2023-11-22 14:37:11 +0100

    chore: tidy up code

---
+++ app/components/Challenges/Challenges.styled.tsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const ChallengesListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+export const ChallengeDiv = styled.div`
+  position: relative;
+  margin-bottom: 20px;
+  width: 280px;
+
+  @media only screen and (device-width: 768px) {
+    /* default iPad screens */
+    width: 17.5em;
+  }
+
+  h2 {
+    font-family: "Sims4Font";
+    font-size: 1.2em;
+    margin: 10px 0px 2px 0px;
+  }
+`;
+
+export const TypeTag = styled.div`
+  position: absolute;
+  top: 125px;
+  left: 10px;
+  display: flex;
+  border-radius: 15px;
+  padding: 5px 12px;
+  align-items: center;
+  justify-content: center;
+  background-color: #9edcf5;
+  color: #155670;
+  font-size: 13px;
+  z-index: 100;
+`;
+
+export const ChallengeImageContainer = styled.div`
+  width: 18.75em;
+  height: 10.625em;
+
+  .image {
+    border-radius: 15px;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  @media screen and (min-width: 1000px) and (max-width: 1200px) {
+    width: 20em;
+    height: 12em;
+  }
+`;
