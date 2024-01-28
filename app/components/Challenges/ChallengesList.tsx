8c0e0e2481b03632812d8897472d23a6a27b7eb6
Henrike Stahlhut <henrike.st@icloud.com>
2024-01-28 14:59:17 +0100

    chore(detailpages): add links to detailpages

--- app/components/Challenges/ChallengesList.tsx
+++ app/components/Challenges/ChallengesList.tsx
@@ -6,23 +6,26 @@ import {
   ChallengeImageContainer,
 } from "./Challenges.styled";
 import { challenges } from "@/lib/challenges";
+import Link from "next/link";
 
 export default function ChallengesList() {
   return (
     <ChallengesListContainer>
       {challenges.map((challenge, index) => (
         <ChallengeDiv key={index}>
-          <TypeTag>{challenge.type}</TypeTag>
-          <ChallengeImageContainer>
-            <img
-              src={`/${challenge.image}`}
-              alt={challenge.title}
-              className="image"
-            />
-          </ChallengeImageContainer>
+          <Link href={`/challenges/${challenge.slug}`}>
+            <TypeTag>{challenge.type}</TypeTag>
+            <ChallengeImageContainer>
+              <img
+                src={`/${challenge.image}`}
+                alt={challenge.title}
+                className="image"
+              />
+            </ChallengeImageContainer>
 
-          <h2>{challenge.title}</h2>
-          <p>{challenge.teaser}</p>
+            <h2>{challenge.title}</h2>
+            <p>{challenge.teaser}</p>
+          </Link>
         </ChallengeDiv>
       ))}
     </ChallengesListContainer>
